a3782f77580d055c7a58cedf70112b0166667e21
dpetrov <dpetrov@mabius.ru>
2016-10-18 17:04:11 +0300

    Refactoring. Creates type more general

--- src/Control/Monad/Log/Handler.hs
+++ src/Control/Monad/Log/Handler.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE DataKinds #-}
 {-|
@@ -7,18 +8,25 @@ module Control.Monad.Log.Handler where
 
 import Control.Retry (recovering, exponentialBackoff, logRetries, defaultLogMsg)
 import Data.Text (Text)
-import Network.Google (HasEnv, runResourceT, runGoogle, send, Error(TransportError, ServiceError))
+import Network.Google (runResourceT, runGoogle, send, Error(TransportError, ServiceError))
 import Network.Google.Logging
        (MonitoredResource, WriteLogEntriesRequestLabels, LogEntry, entriesWrite, writeLogEntriesRequest)
+import Network.Google.Auth.Scope (HasScope', AllowScopes)
+import Network.Google.Env (HasEnv)
 import Control.Monad.Log (BatchingOptions, Handler, withBatchedHandler)
 import Control.Monad.IO.Class (MonadIO(liftIO))
 import Control.Monad.Catch (MonadMask)
 
+
 -- | `withGoogleLoggingHandler` creates a new `Handler` for flash logs to <https://cloud.google.com/logging/ Google Logging>
 withGoogleLoggingHandler
-    :: (HasEnv '["https://www.googleapis.com/auth/cloud-platform",
-                 "https://www.googleapis.com/auth/logging.admin",
-                 "https://www.googleapis.com/auth/logging.write"] r, MonadIO io, MonadMask io)
+    :: (HasScope' s '["https://www.googleapis.com/auth/cloud-platform",
+                      "https://www.googleapis.com/auth/logging.admin",
+                      "https://www.googleapis.com/auth/logging.write"] ~ 'True
+       ,AllowScopes s
+       ,HasEnv s r
+       ,MonadIO io
+       ,MonadMask io)
     => BatchingOptions
     -> r
     -> Text
@@ -32,9 +40,11 @@ withGoogleLoggingHandler options env logname resource labels =
 
 -- | method for flash log to <https://cloud.google.com/logging/ Google Logging>
 flushToGoogleLogging
-    :: (HasEnv '["https://www.googleapis.com/auth/cloud-platform",
-                 "https://www.googleapis.com/auth/logging.admin",
-                 "https://www.googleapis.com/auth/logging.write"] r)
+    :: (HasScope' s '["https://www.googleapis.com/auth/cloud-platform",
+                                                "https://www.googleapis.com/auth/logging.admin",
+                                                "https://www.googleapis.com/auth/logging.write"] ~ 'True
+       ,AllowScopes s
+       ,HasEnv s r)
     => r
     -> Text
     -> MonitoredResource
